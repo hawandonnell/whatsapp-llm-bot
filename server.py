217a95ccef6a8a0a72f48a5ee14e51842db81131
Altair Ermekbai <altairermekbai@gmail.com>
2025-05-28 17:38:13 +0500

    GET verification endpoint update

--- server.py
+++ server.py
@@ -1,15 +1,21 @@
+import os
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 @app.post("/webhook")
 def webhook_handler(request: dict):
-    print("Received webhook request:", request)
-    
-    return {"status": "success", "message": "Webhook received successfully"}
+    return JSONResponse(content=request, status_code=200)
 
 @app.get("/webhook")
 def webhook_handler(hub):
-    print("Received webhook request:", hub)
+    mode = hub.mode
+    token = hub.verify_token
+    challenge = hub.challenge
+
+    if mode == 'subscribe' and os.getenv('VERIFY_TOKEN') == token:
+        return JSONResponse(content=challenge, status_code=200)
+    else:
+        return JSONResponse(content="Verification failed", status_code=403)
     
-    return {"status": "success", "message": "Webhook received successfully"}
